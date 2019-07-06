3ddd01fa3a8034d2cb3ed737560b7966c45aeda6
MiKinshu <mishraprateekaries@gmail.com>
2019-07-07 01:04:57 +0530

    Added Firebase Auth along with user preferences class

--- app/src/main/java/com/kinshuu/silverbook/MainActivity.java
+++ app/src/main/java/com/kinshuu/silverbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kinshuu.silverbook;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -17,11 +18,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -33,23 +37,26 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements SubjectAdapter.itemclicked, PopupDiaogue.PopupDialogueListener {
 
+    private static final int RC_SIGN_IN = 1;
     PieChart pieChart;//for setting up PieChart in detail frag.
     ArrayList<Subject> subjectsmain=new ArrayList<>();//Main local Subjects ArrayList
     ArrayList<SubjectSync> subjectSyncArrayList=new ArrayList<>();//ArrayList synced from firebase.
 
     Integer indexmain=0;
     Integer firsttime;
+    Integer gotpreference=0;
 
     //For managing userdata.
-    Integer Signed_In=1;
+    Integer Signed_In=0;
     Integer Eligible=0;
-    Integer YearOfJoining=2018;
-    String College="IIIT-A";
-    String Branch="IT";
+    Integer YearOfJoining=0;
+    String College="null";
+    String Branch="null";
     private String mUsername="ANONYMOUS";
     String TAG="MyLOGS";
 
@@ -57,20 +64,50 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference msubjectsDatabaseReference;
     private ChildEventListener mChildEventListener;
+    public static FirebaseAuth mFirebaseAuth;
+    public static FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: Starting OnCreate");
         super.onCreate(savedInstanceState);
-        if(YearOfJoining>2017&&College.equals("IIIT-A")&&Signed_In==1)
-            Eligible=1;
         setContentView(R.layout.activity_main);
 
-        //Checking if the user has signed in for the first time.
+        //Getting user preferences.
         SharedPreferences faveditor=getSharedPreferences("com.kinshuu.silverbook.ft",MODE_PRIVATE);
         firsttime=faveditor.getInt("FT",1);
+        faveditor=getSharedPreferences("com.kinshuu.silverbook.ref",MODE_PRIVATE);
+        College=faveditor.getString("College","null");
+        Toast.makeText(this, "College is "+College, Toast.LENGTH_SHORT).show();
+        Branch=faveditor.getString("Branch","null");
+        Toast.makeText(this, "Branch is "+Branch, Toast.LENGTH_SHORT).show();
+        YearOfJoining=faveditor.getInt("YearOfJoining",0);
+
+        if(College.equals("null")||Branch.equals("null")||YearOfJoining==0) {
+            Log.d(TAG, "onCreate: Going to take preference from user!");
+            Intent intent = new Intent(MainActivity.this, com.kinshuu.silverbook.UserOrientation.class);
+            startActivityForResult(intent, 23);
+            gotpreference=0;
+        }
+
+        /*College="IIIT-A";
+        Branch="IT";
+        YearOfJoining=2018;
+
+        Log.d(TAG, "onCreate: VA College is "+College);
+        Log.d(TAG, "onCreate: VA Branch is "+Branch);
+        Log.d(TAG, "onCreate: VA YearOfJoining is "+YearOfJoining);*/
+
+        if(YearOfJoining>2017&&College.equals("IIIT-A"))
+            Eligible=1;
 
         mFirebaseDatabase=FirebaseDatabase.getInstance();
+        mFirebaseAuth=FirebaseAuth.getInstance();
         msubjectsDatabaseReference=mFirebaseDatabase.getReference().child(College).child(YearOfJoining.toString()).child(Branch);
+        Log.d(TAG, "onCreate: Present Database Reference is");
+        Log.d(TAG, "onCreate: College is "+College);
+        Log.d(TAG, "onCreate: Branch is "+Branch);
+        Log.d(TAG, "onCreate: YearOfJoining is "+YearOfJoining);
 
         subjectSyncArrayList=SyncData();
         subjectsmain = LoadData();
@@ -85,9 +122,6 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
         bundle.putParcelableArrayList("arraylist",subjectsmain);
         bundle.putInt("elegible",Eligible);
         Objects.requireNonNull(listFrag).getArgs(bundle);
-
-        if(firsttime==1)
-            Toast.makeText(this, "Hang in tight, while we load your data.", Toast.LENGTH_SHORT).show();
 
         //Following code hides detail frag in portrait mode.
         if(findViewById(R.id.layout_portrait)!=null) {
@@ -105,57 +139,35 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
                 setdetailfrag(0);
         }
 
-        //following code is to manage the firebase sync data.
-        final ArrayList<SubjectSync> subjectSyncArrayListtemp=new ArrayList<>();
-        mChildEventListener=new ChildEventListener() {
+        mAuthStateListener=new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                SubjectSync subject= dataSnapshot.getValue(SubjectSync.class);
-                subjectSyncArrayListtemp.add(subject);
-                Log.d(TAG, "onChildAdded: New child added");
-            }
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-        msubjectsDatabaseReference.addChildEventListener(mChildEventListener);
-        msubjectsDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                subjectSyncArrayList=subjectSyncArrayListtemp;
-                if(firsttime==1){
-                    //changing firsttime to 0.
-                    SharedPreferences.Editor faveditor=getSharedPreferences("com.kinshuu.silverbook.ft",MODE_PRIVATE).edit();
-                    faveditor.putInt("FT",0);
-                    faveditor.apply();
-                    Log.d(TAG, "onDataChange: Saved shared preference");
-
-                    subjectsmain=InitialiseSub();
-                    Log.d(TAG, "onDataChange: initialised subjects main");
-                    recreate();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user= firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    //user is signed in.
+                    Toast.makeText(MainActivity.this, "Welcome to the App you Son of a Gun", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onAuthStateChanged: User is signed in");
+                    OnSignedInInitialise(user.getDisplayName());
+                }
+                else{
+                    //user is signed out.
+                    Log.d(TAG, "onAuthStateChanged: User is signed out.");
+                    OnSignedOutInitialise();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)//Experiment with this.
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
                 }
             }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        };
+        Log.d(TAG, "onCreate: OnCreate ends");
     }
+
 
     //This function is used to update the visibility of views as per the constraints.
     private void UpdateViewVisibility(int i) {
@@ -181,7 +193,7 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
             group4.setVisibility(View.GONE);
             group5.setVisibility(View.GONE);
             groupGPI.setVisibility(View.GONE);
-            TVGPAhead.setText("GPA forcast is only available for IIIT-A batch 2k18 or later. If you belong to the group, please Sign-In.");
+            TVGPAhead.setText("GPA forcast is only available for IIIT-A batch 2k18 or later.");
         }
         else{
             TextView TVtestnames=findViewById(R.id.TVtestnames);
@@ -298,6 +310,7 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
 
     //This Function is used to set up subjects for an eligible user when he signs in for the 1st time.
     private ArrayList<Subject> InitialiseSub() {
+        Log.d(TAG, "InitialiseSub: Starts");
         ArrayList<Subject> subjects=new ArrayList<>();
         if(Eligible==1) {
             for (int i = 0; i < subjectSyncArrayList.size(); i++)
@@ -307,7 +320,9 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
         else {
             subjects.add(new Subject("Enter your"));
             subjects.add(new Subject("subjects"));
+            Log.d(TAG, "InitialiseSub: User not eligible therefore loading 2 default subjects.");
         }
+        Log.d(TAG, "InitialiseSub: Exitting InitialiseSub");
         return subjects;
     }
 
@@ -655,7 +670,7 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
         String json1=gson1.toJson(subjectSyncArrayList);
         editor1.putString("subjectssynclist",json1);
         editor1.apply();
-        Log.d(TAG, "SaveData: ArrayLists saved!");
+        Log.d(TAG, "SaveData: ArrayLists saved!+ size of synclist is "+subjectSyncArrayList.size());
     }
 
     //This function loads data from the local storage.
@@ -669,11 +684,13 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
         if(subjectsmain==null){
             subjectsmain=new ArrayList<>();
         }
+        Log.d(TAG, "LoadData: Loaded Subjects main from disk and size is "+subjectsmain.size());
         return subjectsmain;
     }
 
     //This function is used to set up SubjectsSync for an eligible user.
     private ArrayList<SubjectSync> SyncData() {
+        Log.d(TAG, "SyncData: Eligible is "+Eligible);
         ArrayList<SubjectSync> list=new ArrayList<>();
         if(Eligible==1) {
             SharedPreferences SPSubjectsSync = Objects.requireNonNull(this).getSharedPreferences("SubjectsSyncArrayList",MODE_PRIVATE);
@@ -684,41 +701,154 @@ public class MainActivity extends AppCompatActivity implements SubjectAdapter.it
         }
         if(list==null)
             list=new ArrayList<>();
+        Log.d(TAG, "SyncData: Data loaded from disk and size of SubjectSync is "+list.size());
         return list;
     }
 
     @Override
     protected void onPause() {
+        Log.d(TAG, "onPause: OnPause Starts");
         SaveData();
         super.onPause();
+        if(mAuthStateListener!=null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+            Log.d(TAG, "onPause: Detached AuthStateListener");
+        }
+        if(mChildEventListener!=null) {
+            msubjectsDatabaseReference.removeEventListener(mChildEventListener);
+            Log.d(TAG, "onPause: DetachedChildEventListener");
+            mChildEventListener = null;
+        }
+        Log.d(TAG, "onPause: Exitting OnPause.");
+    }
+
+    @Override
+    protected void onResume(){
+        Log.d(TAG, "onResume: OnResume starts");
+        super.onResume();
+        if(gotpreference==1||firsttime==0) {
+            mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+            Log.d(TAG, "onResume: Attached AuthStateListener");
+        }
+        Log.d(TAG, "onResume: OnResume Ends");
+    }
+
+
+    private void OnSignedOutInitialise() {
+        Signed_In=0;
+        mUsername="ANONYMOUS";
+        if(mChildEventListener!=null) {
+            msubjectsDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
+    }
+
+    private void OnSignedInInitialise(String displayName) {
+        Log.d(TAG, "OnSignedInInitialise: Starts");
+        mUsername=displayName;
+        attachDatabaseReadListener();
+        Log.d(TAG, "OnSignedInInitialise: Attached DatabaseListener");
+        Log.d(TAG, "OnSignedInInitialise: Ends");
+    }
+
+    private void attachDatabaseReadListener() {
+        //following code is to manage the firebase sync data.
+        Log.d(TAG, "attachDatabaseReadListener: Starts");
+        final ArrayList<SubjectSync> subjectSyncArrayListtemp = new ArrayList<>();
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    SubjectSync subject = dataSnapshot.getValue(SubjectSync.class);
+                    subjectSyncArrayListtemp.add(subject);
+                    Log.d(TAG, "onChildAdded: New child added");
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+            msubjectsDatabaseReference.addChildEventListener(mChildEventListener);
+            msubjectsDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Toast.makeText(MainActivity.this, "Data Updated :)", Toast.LENGTH_SHORT).show();
+                    subjectSyncArrayList = subjectSyncArrayListtemp;
+                    Log.d(TAG, "onDataChange: Data Synced from cloud");
+                    if (firsttime == 1) {
+                        //changing firsttime to 0.
+                        SharedPreferences.Editor faveditor = getSharedPreferences("com.kinshuu.silverbook.ft", MODE_PRIVATE).edit();
+                        faveditor.putInt("FT", 0);
+                        faveditor.apply();
+                        Log.d(TAG, "onDataChange: Saved shared preference");
+                        Log.d(TAG, "onDataChange: Firsttime==1 and thus going to InitialiseSub");
+                        subjectsmain = InitialiseSub();
+                        Log.d(TAG, "onDataChange: initialised subjects main");
+                        recreate();
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(TAG, "onActivityResult: In ActivityResult.");
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==23){
+            Log.d(TAG, "onActivityResult: Recieved User Preference!");
+            if(resultCode==RESULT_OK){
+                Branch=data.getStringExtra("Branch");
+                College=data.getStringExtra("College");
+                YearOfJoining=data.getIntExtra("Batch",0);
+                msubjectsDatabaseReference=mFirebaseDatabase.getReference().child(College).child(YearOfJoining.toString()).child(Branch);
+                SharedPreferences.Editor faveditor = getSharedPreferences("com.kinshuu.silverbook.ref", MODE_PRIVATE).edit();
+                faveditor.putString("Branch", Branch);
+                faveditor.putString("College",College);
+                faveditor.putInt("YearOfJoining",YearOfJoining);
+                faveditor.apply();
+                gotpreference=1;
+                if(YearOfJoining>2017&&College.equals("IIIT-A"))
+                    Eligible=1;
+                Log.d(TAG, "In MainActivity class College, Branch and Batch recieved are "+College+","+Branch+","+YearOfJoining);
+            }
+            if(resultCode==RESULT_CANCELED){
+                Toast.makeText(this, "Cannot work until you provide given info", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+        if(requestCode==RC_SIGN_IN){
+            if(resultCode==RESULT_CANCELED) {
+                SharedPreferences.Editor faveditor = getSharedPreferences("com.kinshuu.silverbook.ref", MODE_PRIVATE).edit();
+                faveditor.putString("Branch", "null");
+                faveditor.putString("College", "null");
+                faveditor.putInt("YearOfJoining", 0);
+                faveditor.apply();
+                Toast.makeText(this, "Cannot work until you Sign-In", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+        Log.d(TAG, "onActivityResult: Exiting ActivityResult");
     }
 }
-
-
-
-
-
-            /*ArrayList<Integer> topperScore=new ArrayList<>();
-            topperScore.add(41);
-            ArrayList<String> testName=new ArrayList<>();
-            testName.add("C1");
-            ArrayList<Integer> maxScore = new ArrayList<>();
-            maxScore.add(12);
-            ArrayList<Integer> ScoresToConsider=new ArrayList<>();
-            ScoresToConsider.add(1);
-            ScoresToConsider.add(-1);
-            ScoresToConsider.add(-1);
-            ScoresToConsider.add(-1);
-            ScoresToConsider.add(-1);
-            list.add(new SubjectSync("DST", 1, "Enter marks for C1", topperScore, testName, maxScore,ScoresToConsider));
-            topperScore.add(11);
-            testName.add("Quiz-2");
-            maxScore.add(16);
-            ArrayList<Integer> ScoresToConsider2=new ArrayList<>();
-            ScoresToConsider2.add(1);
-            ScoresToConsider2.add(1);
-            ScoresToConsider2.add(-1);
-            ScoresToConsider2.add(-1);
-            ScoresToConsider2.add(-1);
-            list.add(new SubjectSync("UMC", 2, "Enter marks for C1, Quiz-2", topperScore, testName, maxScore,ScoresToConsider2));*/
 
---
+++ app/src/main/java/com/kinshuu/silverbook/UserOrientation.java
@@ -0,0 +1,95 @@
+package com.kinshuu.silverbook;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class UserOrientation extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String College="null";
+    String Branch="null";
+    Integer Batch=0;
+    Button BTNcontinue;
+
+    String TAG="MyLOGS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_orientation);
+
+        Spinner spinnerCollege= findViewById(R.id.spinnerCollege);
+        Spinner spinnerBatch=findViewById(R.id.spinnerBatch);
+        Spinner spinnerBranch=findViewById(R.id.spinnerBranch);
+        BTNcontinue=findViewById(R.id.BTNcontinue);
+
+        String[] spinnerBatchlist={"2018","2017","2019","Others"};
+        ArrayAdapter<String> spinnerBatchAdapter= new ArrayAdapter<>(UserOrientation.this, android.R.layout.simple_list_item_1,spinnerBatchlist);
+        spinnerBatchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBatch.setAdapter(spinnerBatchAdapter);
+        spinnerBatch.setOnItemSelectedListener(this);
+
+        String[] spinnerBranchlist={"IT","ECE","Others"};
+        ArrayAdapter<String> spinnerBranchAdapter= new ArrayAdapter<>(UserOrientation.this, android.R.layout.simple_list_item_1,spinnerBranchlist);
+        spinnerBranchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBranch.setAdapter(spinnerBranchAdapter);
+        spinnerBranch.setOnItemSelectedListener(this);
+
+        String[] spinnerCollegelist={"IIIT-A", "Others"};
+        ArrayAdapter<String> spinnerCollegeAdapter= new ArrayAdapter<>(UserOrientation.this, android.R.layout.simple_list_item_1,spinnerCollegelist);
+        spinnerCollegeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCollege.setAdapter(spinnerCollegeAdapter);
+        spinnerCollege.setOnItemSelectedListener(this);
+
+        BTNcontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.putExtra("College",College);
+                intent.putExtra("Branch",Branch);
+                intent.putExtra("Batch",Batch);
+                setResult(RESULT_OK,intent);
+                Log.d(TAG, "In User Orientation class onClick: College, Branch and Batch is "+College+","+Branch+","+Batch);
+                UserOrientation.this.finish();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()){
+
+            case R.id.spinnerBatch:{
+                Batch=Integer.parseInt(parent.getItemAtPosition(position).toString());
+                Toast.makeText(this, Batch+"", Toast.LENGTH_SHORT).show();
+                Log.d("UserOrientation", "onItemSelected: "+Batch);
+                break;
+            }
+            case R.id.spinnerCollege:{
+                College=parent.getItemAtPosition(position).toString();
+                Toast.makeText(this, College, Toast.LENGTH_SHORT).show();
+                Log.d("UserOrientation", "onItemSelected: "+College);
+                break;
+            }
+            case R.id.spinnerBranch:{
+                Branch=parent.getItemAtPosition(position).toString();
+                Toast.makeText(this, Branch, Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
