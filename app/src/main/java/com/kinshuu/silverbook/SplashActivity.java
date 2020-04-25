06ee32d38f93acb3ec0eb0737efdab6291db9053
phantom654 <sumitkumarsahu01586@gmail.com>
2020-04-25 15:20:34 +0530

    fixed Splash Screen Landscape issue

--- app/src/main/java/com/kinshuu/silverbook/SplashActivity.java
+++ app/src/main/java/com/kinshuu/silverbook/SplashActivity.java
@@ -2,15 +2,26 @@ package com.kinshuu.silverbook;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Start home activity
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        // close splash activity
-        finish();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start home activity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                // close splash activity
+                finish();
+
+            }
+        }, 1000);
+
     }
 }
